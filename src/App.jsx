78048b011d2a0bce9460bee87601b6dfc974487a
18gaurav2021 <18gaurav2021@gmail.com>
2025-04-16 14:06:50 +0530

    tic_tac_toe game pushed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import TicTacToe from "./tictactoe";
+
+function App() {
+  return (
+    <>
+      <TicTacToe />
+    </>
+  );
+}
+
+export default App;
